bf8d660fd53a896e578cb40b5dc995e88213a75d
Monica Sirathanarun <monicasirathanarun@Steves-iMac.local>
2018-09-28 18:50:41 -0700

    fixing intersect

--- array_intersect.rb
+++ array_intersect.rb
@@ -2,32 +2,46 @@
 #use sorting?
 def intersect(nums1, nums2)
 	#sort nums1
-	nums1.sort!
-	nums2.sort!
-	#Now that it is sorted, I can find where they intersect by iterating through it once
-	nl1 = nums1.length
-	nl2 = nums2.length
-	ml = [nl1,nl2].max
 	arr = []
-	j = 0
-	k = 0
-	p nums1
-	p nums2
-	(0...ml).each do |i|
-		#var j,k
-		if nums1[j] == nums2[k]
-			arr << nums1[j]
-			j += 1
-			k += 1
-		elsif nums1[j] < nums2[k]
-			j += 1
-		else
-			k += 1
+	if nums1.empty? || nums2.empty?
+		return []
+	else
+		nums1.sort!
+		nums2.sort!
+		#Now that it is sorted, I can find where they intersect by iterating through it once
+		nl1 = nums1.length
+		nl2 = nums2.length
+		ml = [nl1,nl2].max
+		j = 0
+		k = 0
+		p nums1
+		p nums2
+		while j != nl1-1 && k != nl2-1
+			#var j,k
+			if nums1[j] == nums2[k]
+				arr << nums1[j]
+				if j < nl1-1 
+					j += 1
+				end
+				if k < nl2-1
+					k += 1
+				end
+			elsif nums1[j] < nums2[k]
+				if j < nl1-1
+					j += 1
+				end
+			else
+				if k < nl2-1
+					k += 1
+				end
+			end
 		end
-		p arr
 	end
-	return arr
+	p arr
 	
 end
 
-p intersect([1,2,2,1],[2,2])
+intersect([1,1],[1])
+			p arr
+			p "j #{j}"
+			p "k #{k}"
